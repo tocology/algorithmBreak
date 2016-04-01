9caf9c22aaa0a56fa27302ad9ece70dca53d4e48
JunHo Hwang <hwangjun7777@gmail.com>
2016-04-01 11:46:13 +0000

    feat: Modify GoodSequence.cpp

--- src/GoodSequence.cpp
+++ src/GoodSequence.cpp
@@ -9,45 +9,53 @@ int N;
 bool solve(char *arr, int n){
     
     // base condition
-    if(n == N) return;
-    
+    if(n == N) return true;
+        
     bool ret = false;
     for(int i=1; i<=3; ++i){
-        if(arr[n-1] == arr[n]) continue;
+        if(arr[n-1] == (i+'0')) continue;
         if(n > 2) {
             int index = n-2; // same point
             int count = 1;
             while(index >= 0){
-                if(arr[index] == arr[n]){
+                if(arr[index] == i+'0'){
                     break;
                 }
                 index--;
                 count++;
             }
             if(index < 0){ // there is no same point
-                arr[n] = i;
+                arr[n] = i+'0';
                 ret = solve(arr, n+1);
                 if(ret) break;
                 else continue;
             } else if( index - count >= 0 ){
                 bool flag = true;
-                for(int j=n-1, int k=index-1, int cnt =0; cnt < count; ++cnt, --j, --k){
+                for(int j=n-1, k=index-1, cnt =0; cnt < count; ++cnt, --j, --k){
                     if(arr[j] == arr[k]) flag = false;
-                    else flag = true;
+                    else {
+                    	flag = true;
+						break;	
+					}
                 }
                 if(flag) {
-                    arr[n] = i;
+                    arr[n] = i+'0';
                     ret = solve(arr, n+1);
                     if(ret) break;
                     else continue;
-                }
+                } else continue;
             } else {
-                arr[n] = i;
+                arr[n] = i+'0';
                 ret = solve(arr, n+1);
                 if(ret) break;
                 else continue;
             }
-        }
+        } else {
+        	arr[n] = i+'0';
+            ret = solve(arr, n+1);
+            if(ret) break;
+            else continue;
+		}
         
         if(ret) break;
     }
@@ -60,10 +68,10 @@ int main() {
     
     scanf("%d", &N);
     
-    char *arr = (char*)malloc(sizeof(char)*N);
+    char *arr = (char*)malloc(N);
     
-    arr[0] = '0';
-    solve(arr, N-1);
+    arr[0] = '1';
+    solve(arr, 1);
     
     printf("%s", arr);
     
