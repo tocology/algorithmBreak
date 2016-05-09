bd36914e0d4d914fe5579828d355a0f2800c0588
tocology <hwangjun7777@gmail.com>
2016-05-09 22:51:52 +0900

    chore

--- src/quickSort.cpp
+++ src/quickSort.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+void partition(int[] arr, int low, int middle, int high){
+
+}
+
+void quickSort(int[] arr, int low, int high){
+
+    if(low < high){
+    }
+}
+
 int main(){
 
     return 0;
