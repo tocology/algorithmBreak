47bd0fd3da9efd67c339c6367bed0ce58d4a1fb6
tocology <hwangjun7777@gmail.com>
2016-05-11 23:34:44 +0900

    feat: Finish QuickSort.cpp

--- src/quickSort.cpp
+++ src/quickSort.cpp
@@ -2,13 +2,30 @@
 
 using namespace std;
 
-void partition(int[] arr, int low, int middle, int high){
+void partition(int[] arr, int low, int high){
+    int pivot = arr[(low+hight) / 2];
+    while(low <= high){
+        while(arr[low] < pivot) low++;
+        while(arr[right] > pivot) high--;
 
+        if(low <= high) {
+            int tmp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = tmp;
+            low++;
+            high--;
+        }
+    }
+    return low;
 }
 
 void quickSort(int[] arr, int low, int high){
-
-    if(low < high){
+    int index = partition(arr, low, high);
+    if(low < index - 1){
+        quickSort(arr, low, index - 1);
+    }
+    if(index < right){
+        quickSort(arr, index, right);
     }
 }
 
